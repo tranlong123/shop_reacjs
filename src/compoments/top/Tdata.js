0a186427f859d823a85f52f36571387cd90bf840
Tran Long <tranlonghazzi@gmail.com>
2023-07-02 00:16:33 +0700

    topcate  ok

---
+++ src/compoments/top/Tdata.js
@@ -0,0 +1,36 @@
+import headphone from '../../assets/images/top/headphone.png'
+import watch from '../../assets/images/top/watch.png'
+import sunglass from '../../assets/images/top/sunglass.png'
+import watch2 from '../../assets/images/top/watch2.png'
+import sunglass2 from '../../assets/images/top/sunglass2.png'
+
+const Tdata = [
+    {
+      cover: headphone,
+      para: "headphone",
+      desc: "3k orders this week",
+    },
+    {
+      cover: watch,
+      para: "watch",
+      desc: "4k orders this week",
+    },
+    {
+      cover: sunglass,
+      para: "sunglass",
+      desc: "6k orders this week",
+    },
+    {
+      cover: watch2,
+      para: "watch",
+      desc: "4k orders this week",
+    },
+    {
+      cover: sunglass2,
+      para: "sunglass",
+      desc: "6k orders this week",
+    },
+  ]
+  
+  export default Tdata
+  
--- src/compoments/top/TopCate.jsx
+++ src/compoments/top/TopCate.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import TopCart from './TopCart'
+import './styles.css'
+
 
 const TopCate = () => {
-  return (
-    <div>TopCate</div>
-  )
+    return (
+        <>
+            <section className='topCat background'>
+                <div className="container">
+                    <div className="heading d_flex">
+                        <div className="heading-left row f_flex">
+                            <i className="fa fa-border-all"></i>
+                            <h2>Top Categories</h2>
+                        </div>
+                        <div className="heading-right row">
+                            <span>View All</span>
+                            <i className="fa-solid fa-caret-right"></i>
+                        </div>
+                    </div>
+                    <TopCart/>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default TopCate
